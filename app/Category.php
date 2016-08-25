daf87433d10610c10352ba9498c8d17427f5bed0
Tirdea Mihai <tirdea.mihai@gmail.com>
2016-08-25 18:35:40 +0000

    add category to algolia search

--- app/Category.php
+++ app/Category.php
@@ -3,13 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Category extends Model
 {
+
+	use Searchable;
     protected $fillable = ['title', 'slug', 'color'];
+
+
 
     public function getRouteKeyName()
     {
     	return 'slug';
+    }
+
+    public function toSearchableArray()
+    {
+    	
+    	return [
+    	'title' => $this->title,
+    	'slug'  => $this->slug
+    	];
     }
 }
